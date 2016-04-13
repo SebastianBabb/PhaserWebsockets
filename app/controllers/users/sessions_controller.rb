9b3ef3c8319473a2a3a276611aa1bb102507e27e
Sebastian Babb <sebastianbabb@gmail.com>
2016-04-13 11:12:04 -0700

    added redirect to sessions_controller.rb

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,7 @@ class Users::SessionsController < Devise::SessionsController
 
     def after_sign_in_path_for(resource_or_scope)
           # your_path
+          '/lobby/index'
      end
 
     def after_sign_out_path_for(resource_or_scope)
