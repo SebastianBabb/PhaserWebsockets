7b25b7b003a28c43a69c3312560ff1e90e9a6e14
Sebastian Babb <sebastianbabb@gmail.com>
2016-05-08 11:41:24 -0700

    added logs to Game.js to debug

--- app/assets/javascripts/twisted-metal-src/Game.js
+++ app/assets/javascripts/twisted-metal-src/Game.js
@@ -246,7 +246,7 @@ TwistedMetal.Game.prototype = {
                 // Remove the tank sprite from game play.
                 this.clients[delete_tank.id].remove();
                 
-                this.clients[delete_tank.id] = null;
+                //this.clients[delete_tank.id] = null;
                 
                 // Delete the tank from the tanks hash.
                 delete this.clients[delete_tank.id];
